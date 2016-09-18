72107cae852e4d56a83c578f13278b2423cc8db0
Александр <@MORCOMP>
2016-09-18 14:36:01 +0300

    Добавьте файлы проекта.

---
+++ MoreLess/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoreLess.MvcApplication" Language="C#" %>
---
+++ MoreLess/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoreLess.Startup))]
+namespace MoreLess
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
